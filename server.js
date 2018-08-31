e702257f8506d15b3b842f925a2ca8a9cd71a46e
Noah <noah@gangplankhq.com>
2018-08-31 10:05:29 -0700

    pushing up file structure

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+//   mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"); 
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
+  
