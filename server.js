8078d1a9c92a2444d259889e435a1c1594c63f85
Noah <noah@gangplankhq.com>
2018-08-31 12:34:02 -0700

    added api's

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+const routes = require("./routes")
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,9 +12,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
 
+app.use(routes)
+
 //   mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"); 
 
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  });
-  
+});
